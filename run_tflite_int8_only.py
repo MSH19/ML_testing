d503825b46da759a75bb0c0ad2db17bbf51e262c
Mahdi Saleh <48448186+MSH19@users.noreply.github.com>
2024-07-31 12:41:31 +0100

    Update run_tflite_int8_only.py

--- run_tflite_int8_only.py
+++ run_tflite_int8_only.py
@@ -3,11 +3,6 @@ import numpy as np
 import tensorflow as tf
 import os
 import time
-import matplotlib.pyplot as plt
-import pandas as pd
-from scipy import signal
-import math
-import scipy.io
 
 # get the current working directory
 current_path = os.getcwd()
